202852f3416aa699a67d598742c4b8fac4867182
suhanighorpade <64584766+suhanighorpade@users.noreply.github.com>
2020-12-03 14:34:47 +0530

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.set("debug",true);
 mongoose.Promise=Promise;
-mongoose.connect("mongodb+srv://suhanighorpade:138272board@cluster0-oindo.mongodb.net/forex?retryWrites=true&w=majority",{
+mongoose.connect("mongodb+srv://email:password@cluster0-oindo.mongodb.net/forex?retryWrites=true&w=majority",{
     useNewUrlParser: true,
     useUnifiedTopology: true 
 });
